9ebb635a91fbbfeaa6bd24399af78b6526e5bd2c
Charles Rafael <charlesclemente2@gmail.com>
2024-03-25 19:23:29 -0300

    first commit

---
+++ js/dados.js
@@ -0,0 +1,442 @@
+var MENU = {
+    "churrasco": [
+        {
+            "id": "ribs-brisket-and-burnt-ends",
+            "img": "./img/cardapio/burguers/fran.jpg",
+            "name": "Frango com Catupiry",
+            "dsc": "Joe's KC Ribs, Brisket & Burnt Ends",
+            "price": 6.99
+        },
+        {
+            "id": "005-kings-carolina-oink-sampler",
+            "img": "./img/cardapio/burguers/carne.jpg",
+            "name": "Carne Moída",
+            "dsc": "Carolina BBQ Oink Sampler",
+            "price": 6.99
+        },
+        {
+            "id": "texas-monthlys-1-bbq-brisket",
+            "img": "./img/cardapio/burguers/pi.jpg",
+            "name": "Pizza",
+            "dsc": "Texas Monthly's #1 BBQ Brisket",
+            "price": 6.99
+        },
+        {
+            "id": "whole-brisket-texas-bbq-sauce",
+            "img": "./img/cardapio/burguers/margarita.jpg",
+            "name": "Marguerita",
+            "dsc": "Whole Brisket + Texas Barbecue BBQ Sauce",
+            "price": 6.99
+        },
+        {
+            "id": "whole-texas-smoked-brisket",
+            "img": "./img/cardapio/burguers/at.jpg",
+            "name": "Atum",
+            "dsc": "Whole Texas Smoked Brisket",
+            "price": 6.99
+        },
+        {
+            "id": "mini-trinity-bbq-combo-brisket-ribs-and-links",
+            "img": "./img/cardapio/burguers/bro.jpg",
+            "name": "Bróculis",
+            "dsc": "Mini Trinity BBQ Combo - Brisket, Ribs & Links",
+            "price": 6.99
+        },
+        {
+            "id": "235203-blue-smoke-baby-back-ribs-backyard-barbecue-chicken-combo",
+            "img": "./img/cardapio/burguers/calab.jpg",
+            "name": "Calabresa",
+            "dsc": "Blue Smoke Baby Back Ribs + Backyard Barbecue Chicken Combo",
+            "price": 6.99
+        },
+        {
+            "id": "006-kings-meat-lovers-special",
+            "img": "./img/cardapio/burguers/hamb.jpg",
+            "name": "Burguer",
+            "dsc": "BBQ Meat Lovers Special for 10",
+            "price": 6.99
+        }    
+        
+    ],
+    "burgers": [
+        {
+            "id": "the-gramercy-tavern-burger-4-pack",
+            "img": "./img/cardapio/burguers/frango.jpg",
+            "name": "Frango e Catupiry",
+            "ing": "Frango, Catupiry Scalla,<br> Mussarela",
+            "dsc": "The Gramercy Tavern Burger - 4 Pack",
+            "price": 9.99
+        },
+        {
+            "id": "shake-shack-shackburger-8-pack",
+            "img": "./img/cardapio/burguers/boloo.jpg",
+            "name": "Bolonhesa",
+            "dsc": "Shake Shack ShackBurger® - 8 Pack",
+            "price": 9.99
+        },
+        {
+            "id": "gotts-cheeseburger-kit-for-4",
+            "img": "./img/cardapio/burguers/broculis.jpg",
+            "name": "Brócolis Especial",
+            "dsc": "Gott's Complete Cheeseburger Kit for 4",
+            "price": 9.99
+        },
+        {
+            "id": "le-big-matt-kit-for-6",
+            "img": "./img/cardapio/burguers/pizza.jpg",
+            "name": "Pizza",
+            "dsc": "Le Big Matt Burger Kit for 6",
+            "price": 9.99
+        },
+        {
+            "id": "shake-shack-shackburger-16-pack",
+            "img": "./img/cardapio/burguers/lingui.jpg",
+            "name": "Calabresa",
+            "dsc": "Shake Shack Shackburger® - 16 Pack",
+            "price": 9.99
+        },
+        {
+            "id": "21-usda-prime-burgers-pack-of-18-8oz-each",
+            "img": "./img/cardapio/burguers/marg.jpg",
+            "name": "Marguerita",
+            "dsc": "USDA Prime Burgers - Pack of 18 (8oz each)",
+            "price": 9.99
+        },
+        {
+            "id": "double-stack-burger-kit-for-4",
+            "img": "./img/cardapio/burguers/atum.jpg",
+            "name": "Atum",
+            "dsc": "Double Stack Burger Kit for 4",
+            "price": 9.99
+        },
+        {
+            "id": "goldbelly-burger-bash-pack",
+            "img": "./img/cardapio/burguers/acon.jpg",
+            "name": "Com Acompanhamento",
+            "dsc": "Goldbelly \"Burger Bash\" Pack",
+            "price": 15.00
+        },
+        {
+            "id": "burger-au-poivre-kit-4-pack",
+            "img": "./img/cardapio/burguers/legu.jpg",
+            "name": "Adicionais Cada",
+            "dsc": "Burger Au Poivre Kit - 4 Pack",
+            "price": 1.00
+        }
+        
+       
+        
+    ],
+    "sobremesas": [
+        {
+            "id": "15259-german-chocolate-killer-brownie-tin-pack",
+            "img": "./img/cardapio/burguers/coca2l.jpg",
+            "name": "Coca-Cola 2L",
+            "dsc": "German Chocolate Killer Brownie®",
+            "price": 14.00
+        },
+        {
+            "id": "",
+            "img": "./img/cardapio/burguers/coca1l.jpg",
+            "name": "Coca-Cola 1L",
+            "dsc": "German Chocolate Killer Brownie®",
+            "price": 9.99
+        },
+        {
+            "id": "e",
+            "img": "./img/cardapio/burguers/cocalata.jpg",
+            "name": "Coca-Cola Lata",
+            "dsc": "German Chocolate Killer Brownie®",
+            "price": 6.00
+        },
+        {
+            "id": "jacques-world-famous-chocolate-chip-cookies",
+            "img": "./img/cardapio/burguers/cocamk.jpg",
+            "name": "Coca-Cola 290ml",
+            "dsc": "Jacques' World Famous Chocolate Chip Cookies - 6 Pack",
+            "price": 5.00
+        },
+        {
+            "id": "luigis-original-cannoli-pie",
+            "img": "./img/cardapio/burguers/agua.jpg",
+            "name": "Água 250ml",
+            "dsc": "Original Cannoli Pie",
+            "price": 2.99
+        },
+        {
+            "id": "sea-salted-caramel-swirl-cheesecake",
+            "img": "./img/cardapio/burguers/aguagaz.jpg",
+            "name": "Água Com Gaz",
+            "dsc": "Sea-Salted Caramel Swirl Cheesecake",
+            "price": 3.99
+        },
+        {
+            "id": "brooklyn-blackout-cookie-brownie-combo-pack-2-tins",
+            "img": "./img/cardapio/burguers/suco.jpg",
+            "name": "Suco 1L",
+            "dsc": "Brooklyn Blackout Cookie + Brownie Combo Pack - 2 Tins",
+            "price": 9.99
+        },
+        {
+            "id": "best-seller-cupcake-dozen",
+            "img": "./img/cardapio/burguers/h2.jpg",
+            "name": "H2O 1,5L",
+            "dsc": "Best Seller Cupcake Dozen",
+            "price": 9.99
+        },
+        {
+            "id": "choose-your-own-ice-cream-donuts-6-pack",
+            "img": "./img/cardapio/burguers/red.jpg",
+            "name": "Red Bull 250ml",
+            "dsc": "Choose Your Own Ice Cream Donuts - 6 Pack",
+            "price": 12.99
+        },
+        {
+            "id": "17481-jewish-dessert-3-pack",
+            "img": "./img/cardapio/burguers/vinho.jpg",
+            "name": "Vinhos",
+            "dsc": "Jewish Classics Dessert Pack",
+            "price": 19.99
+        },
+        {
+            "id": "dessert-bar-care-package",
+            "img": "./img/cardapio/burguers/ice.jpg",
+            "name": "Ice",
+            "dsc": "Dessert Bar Care Package",
+            "price": 10.00
+        },
+        {
+            "id": "donut-cookies-12-pack",
+            "img": "./img/cardapio/burguers/caipirinha.jpg",
+            "name": "Caipirinha Limão",
+            "dsc": "Donut Cookies - 12 Pack",
+            "price": 24.99
+        },
+        {
+            "id": "gulab-jamun-ice-cream-cakes-2-pack",
+            "img": "./img/cardapio/burguers/haini.jpg",
+            "name": "Heineken 600ml",
+            "dsc": "Gulab Jamun Ice Cream Cakes - 2 Pack",
+            "price": 13.99
+        },
+        {
+            "id": "jacques-world-famous-chocolate-chip-cookies-12-pack",
+            "img": "./img/cardapio/burguers/skol.jpg",
+            "name": "Skol 600ml",
+            "dsc": "Jacques' World Famous Chocolate Chip Cookies - 12 Pack",
+            "price": 10.00
+        },
+        {
+            "id": "",
+            "img": "./img/cardapio/burguers/brama.jpg",
+            "name": "Brahma 600ml",
+            "dsc": "",
+            "price": 10.00
+        }
+    ],
+    "bebidas": [
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "./img/cardapio/burguers/contra.jpg",
+            "name": "Contra f. Acebolado",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 49.99
+        },
+        {
+            "id": "guys-caliente-margaritas-for-12",
+            "img": "./img/cardapio/burguers/batata.jpg",
+            "name": "Batata 500g",
+            "dsc": "Guy's Caliente Margaritas for 12",
+            "price": 17.99
+        },
+        {
+            "id": "woodford-reserve-mint-julep-syrup",
+            "img": "./img/cardapio/burguers/chader.jpg",
+            "name": "Batata Cheddar-Bacon",
+            "dsc": "Woodford Reserve Mint Julep Syrup",
+            "price": 29.99
+        },
+        {
+            "id": "new-orleans-hurricane-mix",
+            "img": "./img/cardapio/burguers/frio.jpg",
+            "name": "Tábua de Frios",
+            "dsc": "New Orleans Hurricane Mix",
+            "price": 49.99
+        },
+        {
+            "id": "margarita-mix",
+            "img": "./img/cardapio/burguers/calabresa.jpg",
+            "name": "Calabresa Acebolada",
+            "dsc": "Margarita Mix",
+            "price": 29.99
+        },
+        {
+            "id": "woodford-reserve-mint-julep-syrup-2-pack",
+            "img": "./img/cardapio/burguers/pasteis.jpg",
+            "name": "Pasteis",
+            "dsc": "Woodford Reserve Mint Julep Syrup - 2 Pack",
+            "price": 34.99
+        },
+        {
+            "id": "unicorn-parade-milkshake-kit-for-8",
+            "img": "./img/cardapio/burguers/tirinha.jpg",
+            "name": "Tirinhas de Pastel",
+            "dsc": "Unicorn Parade Milkshake Kit for 8",
+            "price": 10.00
+        },
+        {
+            "id": "chickpea-chiller-kit-for-6",
+            "img": "./img/cardapio/burguers/nhoque.jpg",
+            "name": "Nhoque",
+            "dsc": "Chickpea Chiller Kit for 6",
+            "price": 29.99
+        },
+        {
+            "id": "15194-old-honey-barn-mint-julep-mixer-200ml",
+            "img": "./img/cardapio/burguers/especial.jpg",
+            "name": "Especial-Bistrô",
+            "dsc": "Old Honey Barn Mint Julep Mixer - 200ml Flask",
+            "price": 84.99
+        },
+        {
+            "id": "kentucky-derby-mint-julep-gift-set",
+            "img": "./img/cardapio/burguers/tulipa.jpg",
+            "name": "Tulipa",
+            "dsc": "Kentucky Derby Mint Julep Gift Set",
+            "price": 49.99
+        },
+        {
+            "id": "002-charleston-bloody-mary-mix-weekender-bold-and-spicy",
+            "img": "./img/cardapio/burguers/torresmo.jpg",
+            "name": "Torresmo",
+            "dsc": "Weekender | Charleston Bloody Mary Mix Bold & Spicy",
+            "price": 49.99
+        },
+        {
+            "id": "nola-cold-brew-concentrate-bag-in-box",
+            "img": "./img/cardapio/burguers/mist.jpg",
+            "name": "Coxinha-Kibe Meia",
+            "dsc": "NOLA Cold Brew Concentrate Bag-in-Box",
+            "price": 44.99
+        },
+        {
+            "id": "nola-cold-brew-concentrate-bag-in-box",
+            "img": "./img/cardapio/burguers/kibe.jpg",
+            "name": "Kibe Inteira",
+            "dsc": "NOLA Cold Brew Concentrate Bag-in-Box",
+            "price": 49.99
+        }
+        
+    ],
+    "pizzas": [
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/cardapio/burguers/confete.jpg",
+            "name": "Confete",
+            "dsc": "2 Lou Malnati's Deep Dish Pizzas",
+            "price": 9.99
+        },
+        {
+            "id": "23699-choose-your-own-thin-crust-pizza-4-pack",
+            "img": "./img/cardapio/burguers/churros.jpg",
+            "name": "Churros",
+            "dsc": "Choose Your Own Thin Crust Pizza - 4 Pack",
+            "price": 9.99
+        },
+        {
+            "id": "choose-your-own-new-haven-style-pizza-6-pack",
+            "img": "./img/cardapio/burguers/morango.jpg",
+            "name": "Morango",
+            "dsc": "New Haven-S - 6 Pack (Choose Your Own)",
+            "price": 12.99
+        },
+        {
+            "id": "6-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/cardapio/burguers/banana.jpg",
+            "name": "Banana",
+            "dsc": "6 Lou Malnati's Deep Dish Pizzas",
+            "price": 12.99
+        },
+        {
+            "id": "wood-fired-pizzas-best-seller-4-pack",
+            "img": "./img/cardapio/burguers/banana.jpg",
+            "name": "DUO",
+            "dsc": "Wood Fired Pizzas Best Seller - 4 Pack",
+            "price": 9.99
+        },
+        {
+            "id": "236991-choose-your-own-deep-dish-pizza-3-pack",
+            "img": "./img/cardapio/burguers/pacoca.jpg",
+            "name": "Paçoca",
+            "dsc": "Choose Your Own Deep Dish Pizza - 3 Pack",
+            "price": 9.99
+        },
+        {
+            "id": "choose-your-own-detroit-style-pizza-3-pack",
+            "img": "./img/cardapio/burguers/bom.jpg",
+            "name": "Bom Bom",
+            "dsc": "Detroit-Style Pizza - Choose Your Own 3 Pack",
+            "price": 9.99
+        },
+        {
+            "id": "brooklyn-pizza-choose-your-own-5-pack",
+            "img": "./img/cardapio/burguers/romeu.jpg",
+            "name": "Romeu e Julieta",
+            "dsc": "Brooklyn Pizza - Choose Your Own 5 Pack",
+            "price": 9.99
+        },
+        {
+            "id": "choose-your-own-chicago-deep-dish-pizza-4-pack",
+            "img": "./img/cardapio/burguers/panetoni.jpg",
+            "name": "Panetone",
+            "dsc": "Chicago Deep Dish Pizza - 4 Pack",
+            "price": 9.99
+        }
+        
+    ],
+    "steaks": [
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./img/cardapio/burguers/confete (1).jpg",
+            "name": "Confete",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 6.99
+        },
+        {
+            "id": "steaks-and-cakes-date-night-dinner-for-2",
+            "img": "./img/cardapio/burguers/chu.jpg",
+            "name": "Churros",
+            "dsc": "Steaks and Cakes Date Night Dinner for 2",
+            "price": 6.99
+        },
+        {
+            "id": "Prime-holiday-steak-sampler-for-10-12",
+            "img": "./img/cardapio/burguers/ba.jpg",
+            "name": "Banana",
+            "dsc": "Chef Matt's Steak Sampler for 10-12",
+            "price": 6.99
+        },
+        {
+            "id": "bone-in-rib-steak",
+            "img": "./img/cardapio/burguers/mor.jpg",
+            "name": "Morango",
+            "dsc": "Bone-in Rib Steak",
+            "price": 6.99
+        },
+        {
+            "id": "american-wagyu-gold-grade-top-sirloins",
+            "img": "./img/cardapio/burguers/duo (1).jpg",
+            "name": "Duo",
+            "dsc": "American Wagyu Gold Grade Top Sirloins",
+            "price": 6.99
+        },
+        {
+            "id": "2-peter-luger-steak-pack-b",
+            "img": "./img/cardapio/burguers/sor.jpg",
+            "name": "Sorvete",
+            "dsc": "Peter Luger Porterhouse Steaks",
+            "price": 19.99
+        }
+        
+    ]
+}
